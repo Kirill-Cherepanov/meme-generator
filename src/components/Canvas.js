4972fe2704c70665ee1daa42b60bcd07330511ad
Cherepanov Kirill <cherkirr@gmail.com>
2022-07-05 18:03:40 +0300

    feat: prepare to add interactivity to Canvas.js

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,30 +1,45 @@
 import React, { useRef, useState, useEffect } from 'react';
 import TextBox from './TextBox';
 
-const DEFAULT_TEXT = 'Write your text here';
-
 // Should keep track of TextBox position relative to canvas and send it to ->App->ToolSidebar
 // Should keep track of TextBox width and send it to ->App->ToolSidebar
 // Should not be allowed to exit the canvas
-
-// templateData = {
-//   "id": "61579",
-//   "name": "One Does Not Simply",
-//   "url": "https://i.imgflip.com/1bij.jpg",
-//   "width": 568,
-//   "height": 335,
-//   "box_count": 2
-// }
 
 export default function Canvas({
   templateData,
   handleSelection,
   handleModifySidebarProps
 }) {
+  const textBoxRefs = useRef([]);
+  const [x, setX] = useState();
+  const [y, setY] = useState();
+
+  const textBoxes = Array(templateData.box_count)
+    .fill(0)
+    .map((item, index) => (
+      <TextBox
+        key={index}
+        innerRef={(element) => textBoxRefs.current.push(element)}
+      />
+    ));
+
+  const setPosition = (textBoxRef) => {
+    // it might be index of textBoxRefs intead of textBoxRef
+    const x = textBoxRef.current.offsetLeft;
+    const y = textBoxRef.current.offsetTop;
+    setX(x);
+    setY(y);
+    handleModifySidebarProps();
+  };
+
+  useEffect(() => {
+    // on dragging or resizing setPosition()
+  }, []);
+
   return (
     <div className="canvas">
       <img src={templateData.url} alt="Template" />
-      <TextBox>{DEFAULT_TEXT}</TextBox>
+      {textBoxes}
     </div>
   );
 }
