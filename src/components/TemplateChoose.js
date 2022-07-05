acea3968f83be0f1c98e3247f313d15a5ecfa70a
Cherepanov Kirill <cherkirr@gmail.com>
2022-07-05 17:09:34 +0300

    feat: add static TemplateChooose.js

---
+++ src/components/TemplateChoose.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import StyledButton from './StyledButton';
+
+export default function TemplateChoose() {
+  return (
+    <div className="starting">
+      <h1 className="starting__title">Meme generator</h1>
+      <h2 className="starting__desc">
+        Create memes from your own images or use popular templates.
+        <br />
+        Edit your template and generate a meme.
+      </h2>
+      <StyledButton primary={true} size={'L'} clickHandler={() => {}}>
+        Select meme template
+      </StyledButton>
+      <StyledButton primary={false} size={'M'} clickHandler={() => {}}>
+        Upload image
+      </StyledButton>
+    </div>
+  );
+}
+
+// # TemplateChoose
+//   Upload a new template
+//   Choose from popular templates
