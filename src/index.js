8d4cfd310d6f167180521a90fb098d975251a4af
Cherepanov Kirill <cherkirr@gmail.com>
2022-07-27 18:19:17 +0300

    fix: fix mobile keyboard ruining layout issue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
+import isMobile from './utils/isMobile';
+
+if (isMobile) {
+  const metaViewport = document.querySelector('meta[name=viewport]');
+  metaViewport.setAttribute(
+    'content',
+    'height=' + window.innerHeight + 'px, width=device-width, initial-scale=1.0'
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
